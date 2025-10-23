16202bfd8235aca4b9a14efc2ec43d967f23e272
ccarlaa <carlacarlaclementino@gmail.com>
2025-10-23 19:17:10 -0300

    fix padding

--- dashous/src/sections/hero.jsx
+++ dashous/src/sections/hero.jsx
@@ -28,8 +28,8 @@ const Hero = () => {
     }, []);
 
     return (
-        <section ref={sectionRef} className="w-full md:min-h-[500px] lg:min-h-[753px] bg-white flex justify-center relative px-6 lg:px-0 mt-20 lg:mt-40">
-            <div className="w-full lg:max-w-[1024px] md:max-w-[500px] flex flex-col gap-6 lg:gap-10 py-10 lg:py-0">
+        <section ref={sectionRef} className="w-full md:min-h-[500px] lg:min-h-[780px] items-center  bg-white flex justify-center relative px-6 ">
+            <div className="w-full lg:max-w-[1024px] mb-20 md:max-w-[500px] flex flex-col gap-6 lg:gap-10 py-10 lg:py-0">
                 <h1 className={`font-bold text-gray-200  lg:text-left text-center text-lg  md:text-2xl lg:text-5xl w-full lg:w-[680px] tracking-wider lg:tracking-widest transition-all duration-700 ease-out ${
                     isVisible ? 'opacity-100 translate-x-0' : 'opacity-0 -translate-x-10'
                 }`}>
@@ -49,7 +49,7 @@ const Hero = () => {
             <img
                 src={HeroImage}
                 alt="people_working"
-                className={`absolute right-0 bottom-0 hidden lg:block h-auto lg:w-[400px] xl:w-[550px] 2xl:w-auto transition-all duration-1000 ease-out ${
+                className={`absolute right-0 top-10 hidden lg:block h-auto lg:w-[400px] xl:w-[550px] 2xl:w-auto transition-all duration-1000 ease-out ${
                     isVisible ? 'opacity-100 translate-x-0' : 'opacity-0 translate-x-20'
                 }`}
             />
